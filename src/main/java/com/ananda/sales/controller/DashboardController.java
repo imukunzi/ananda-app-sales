a1d291a0c394bfd853242d0072f9c2dee8c15c9a
Mukashyaka Valentine <mukavalentine85@gmail.com>
2024-03-16 12:20:13 +0200

    updated

--- src/main/java/com/ananda/sales/controller/DashboardController.java
+++ src/main/java/com/ananda/sales/controller/DashboardController.java
@@ -21,8 +21,46 @@ public class DashboardController {
 
 	private double SALES_JAN=50000;
 	private double SALES_FEB=0;
+	private double SALES_MARC=0;
+	private double SALES_APR=0;
+	private double SALES_MAY=0;
+	private double SALES_JUN=0;
+	private double SALES_JULY=0;
+	private double SALES_AUG=0;
+	private double SALES_SEPT=0;
+	private double SALES_OCT=0;
+	private double SALES_NOV=0;
+	private double SALES_DEC=0;
 	
 	private double TARGET_SALES_JAN=20000;
+	private double TARGET_SALES_FEB=0;
+	private double TARGET_SALES_MARC=0;
+	private double TARGET_SALES_APR=0;
+	private double TARGET_SALES_MAY=0;
+	private double TARGET_SALES_JUN=0;
+	private double TARGET_SALES_JULY=0;
+	private double TARGET_SALES_AUG=0;
+	private double TARGET_SALES_SEPT=0;
+	private double TARGET_SALES_OCT=0;
+	private double TARGET_SALES_NOV=0;
+	private double TARGET_SALES_DEC=0;
+	
+	private double PROFIT_JAN=60000;
+	private double PROFIT_FEB=0;
+	private double PROFIT_MARC=0;
+	private double PROFIT_APR=0;
+	private double PROFIT_MAY=0;
+	private double PROFIT_JUN=0;
+	private double PROFIT_JULY=0;
+	private double PROFIT_AUG=0;
+	private double PROFIT_SEPT=0;
+	private double PROFIT_OCT=0;
+	private double PROFIT_NOV=0;
+	private double PROFIT_DEC=0;
+	
+	
+	
+	
 	
 	
 	@PostMapping("/dashboard") //Pulling dashboard service
@@ -34,10 +72,42 @@ public class DashboardController {
 		Map<String, Object> response = new HashMap<>(); 
 		response.put("SALES_JAN",SALES_JAN);
 		response.put("SALES_FEB",SALES_FEB);
+		response.put("SALES_MARC",SALES_MARC);
+		response.put("SALES_APR",SALES_APR);
+		response.put("SALES_MAY",SALES_MAY);
+		response.put("SALES_JUN",SALES_JUN);
+		response.put("SALES_JULY",SALES_JULY);
+		response.put("SALES_AUG",SALES_AUG);
+		response.put("SALES_SEPT",SALES_SEPT);
+		response.put("SALES_OCT",SALES_OCT);
+		response.put("SALES_NOV",SALES_NOV);
+		response.put("SALES_DEC",SALES_DEC);
 		
 		response.put("TARGET_SALES_JAN",TARGET_SALES_JAN);
+		response.put("TARGET_SALES_FEB",TARGET_SALES_FEB);
+		response.put("TARGET_SALES_MARC",TARGET_SALES_MARC);
+		response.put("TARGET_SALES_APR",TARGET_SALES_APR);
+		response.put("TARGET_SALES_MAY",TARGET_SALES_MAY);
+		response.put("TARGET_SALES_JUN",TARGET_SALES_JUN);
+		response.put("TARGET_SALES_JULY",TARGET_SALES_JULY);
+		response.put("TARGET_SALES_AUG",TARGET_SALES_AUG);
+		response.put("TARGET_SALES_SEPT",TARGET_SALES_SEPT);
+		response.put("TARGET_SALES_OCT",TARGET_SALES_OCT);
+		response.put("TARGET_SALES_NOV",TARGET_SALES_NOV);
+		response.put("TARGET_SALES_DEC",TARGET_SALES_DEC);
 		
-		
+		response.put("PROFIT_JAN",PROFIT_JAN);
+		response.put("PROFIT_FEB",PROFIT_FEB);
+		response.put("PROFIT_MARC",PROFIT_MARC);
+		response.put("PROFIT_APR",PROFIT_APR);
+		response.put("PROFIT_MAY",PROFIT_MAY);
+		response.put("PROFIT_JUN",PROFIT_JUN);
+		response.put("PROFIT_JULY",PROFIT_JULY);
+		response.put("PROFIT_AUG",PROFIT_AUG);
+		response.put("PROFIT_SEPT",PROFIT_SEPT);
+		response.put("PROFIT_OCT",PROFIT_OCT);
+		response.put("PROFIT_NOV",PROFIT_NOV);
+		response.put("PROFIT_DEC",PROFIT_DEC);
 		
 		
 		return new ResponseEntity<>(response, HttpStatus.OK);
