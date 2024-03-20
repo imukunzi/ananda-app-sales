d929bc27808240eb5628ef47bdd620be3c7cd4ea
Mukashyaka Valentine <mukavalentine85@gmail.com>
2024-03-20 11:32:16 +0200

    updates

--- src/main/java/com/ananda/sales/controller/PrevisionController.java
+++ src/main/java/com/ananda/sales/controller/PrevisionController.java
@@ -1,13 +1,28 @@
 package com.ananda.sales.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ananda.app.model.Previsions;
@@ -23,6 +38,18 @@ public class PrevisionController {
 	
 	@Autowired
 	private PrevisionRepository previsionRepository;
+	
+	private Sort.Direction getSortDirection(String direction) {
+
+		if (direction.equals("asc")) {
+			return Sort.Direction.ASC;
+		} else if (direction.equals("desc")) {
+			return Sort.Direction.DESC;
+		}
+
+		return Sort.Direction.ASC;
+
+	}
 	
 	@PostMapping("/prevision")
 	@PreAuthorize("hasRole('ROLE_MANAGER')")
@@ -75,7 +102,119 @@ public class PrevisionController {
 		}
 
 	}
+	
+	
+	@GetMapping("/prevision")
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public ResponseEntity<Map<String, Object>> getAllPrevisionPage(@RequestParam(required = false) String code,
+			@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "50") int size,
+			@RequestParam(defaultValue = "id,desc") String[] sort) {
+  
+//		System.out.println("========123========"); 
+		
+		try {
+			List<Order> orders = new ArrayList<Order>();
 
+			if (sort[0].contains(",")) {
+				// will sort more than 2 fields
+				// sortOrder="field, direction"
+				for (String sortOrder : sort) {
+					String[] _sort = sortOrder.split(",");
+					orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
+				}
+			} else {
+				// sort=[field, direction]
+				orders.add(new Order(getSortDirection(sort[1]), sort[0]));
+			}
+
+			List<Prevision> prevision = new ArrayList<Prevision>();
+			Pageable pagingSort = PageRequest.of(page, size, Sort.by(orders));
+
+			Page<Prevision> pageTuts;
+			if (code == null)
+				pageTuts = previsionRepository.findAll(pagingSort);
+			else
+				pageTuts = previsionRepository.findByYearContaining(code, pagingSort);
+
+			prevision = pageTuts.getContent();
+
+			Map<String, Object> response = new HashMap<>();
+			response.put("tutorials", prevision);
+			response.put("currentPage", pageTuts.getNumber());
+			response.put("totalItems", pageTuts.getTotalElements());
+			response.put("totalPages", pageTuts.getTotalPages());
+
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println("+++++++++++++++++++++++++++++++++++++" + e);
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+	
+	@GetMapping("/prevision/{id}")
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public ResponseEntity<Prevision> getPrevision(@PathVariable("id") long id) {
+
+		Optional<Prevision> previsionData = previsionRepository.findById(id);
+
+		if (previsionData.isPresent()) {
+			return new ResponseEntity<>(previsionData.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+	
+	@PutMapping("/prevision/{id}")
+	@PreAuthorize("hasRole('ROLE_MANAGER')")
+	public ResponseEntity<String> updatePrevision(@PathVariable("id") long id, @RequestBody Prevision p) {
+
+//		int count = productRepository.checkIfProductExist(p.getName(), p.getCode(), p.getSize(), p.getColor());
+//		
+//		System.out.println("====118==="+count);
+//		
+//		if(count > 0) {
+//			return new ResponseEntity<>("This product exist!", HttpStatus.OK);
+//			
+//		}
+
+		Optional<Prevision> previsionData = previsionRepository.findById(id);
+
+		if (previsionData.isPresent()) {
+
+			Prevision prevision = previsionData.get();
+
+			prevision.setUsername(p.getUsername());
+			prevision.setYear(p.getYear());
+			prevision.setDescription(p.getDescription());
+			prevision.setType(p.getType());
+			prevision.setJanuary(p.getJanuary());
+			prevision.setFebruary(p.getFebruary());
+			prevision.setMarch(p.getMarch());
+			prevision.setApril(p.getApril());
+			prevision.setMay(p.getMay());
+		    prevision.setJune(p.getJune());
+		    prevision.setJuly(p.getJuly());
+		    prevision.setAugostin(p.getAugostin());
+		    prevision.setSeptember(p.getSeptember());
+		    prevision.setOctomber(p.getOctomber());
+		    prevision.setNovember(p.getNovember());
+		    prevision.setDecember(p.getDecember());
+			
+			
+
+			previsionRepository.save(prevision);
+
+			return new ResponseEntity<>("Updated successfully!", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+	
 }
+
+
 
 
--- src/main/java/com/ananda/sales/repository/PrevisionRepository.java
+++ src/main/java/com/ananda/sales/repository/PrevisionRepository.java
@@ -1,14 +1,23 @@
 package com.ananda.sales.repository;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.ananda.sales.model.Prevision;
+import com.ananda.sales.model.Products;
 
 
 public interface PrevisionRepository extends JpaRepository<Prevision, Long> {
+	
+	Page<Prevision> findByYearContaining(String title, Pageable pageable);
+	List<Prevision> findByYearContaining(String title, Sort sort);
 	
 	@Transactional
 	@Query(value = "select count(id) as count from prevision where year =?1 and type=?2 ")
